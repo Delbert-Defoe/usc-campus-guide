1ef20de551aee6d65edeae66aa7f0325e2c9a61d
ShemarB <114029006+ShemarB@users.noreply.github.com>
2023-03-20 00:20:10 -0400

    Revert "Merge branch 'qr_code_scanner_isaiah_weekes' of https://github.com/kingDebo/usc-campus-guide into home_screen_shemar_linden"
    This reverts commit e0a4e18d2a8c0ef458e6efafe69536e4a2526d1a, reversing
    changes made to b71129618b90add464bddb2a7c62d50675b47855.

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,90 @@
-import 'first.dart';
-import 'second.dart';
 import 'package:flutter/material.dart';
+import 'package:ucg/services/router.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Home(),
-      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      onGenerateRoute: AppRouter.generateRoute,
+      initialRoute: "/",
     );
   }
 }
 
-class Home extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
   @override
-  _HomeState createState() => _HomeState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _HomeState extends State<Home> {
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  final pages = <Map<String, String>>[
+    {"pageTitle": "Onboarding", "route": "/onboarding"},
+    {"pageTitle": "Home", "route": "/"},
+    {"pageTitle": "Map", "route": "/map"},
+    {"pageTitle": "Search", "route": "/search"},
+    {"pageTitle": "Building", "route": "/building"},
+    {"pageTitle": "Qr Code", "route": "/scans"},
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
+    return Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.indigo[900],
-          bottom: const TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(
-                  Icons.qr_code,
-                  size: 40,
-                ),
-              ),
-              Tab(
-                icon: Icon(
-                  Icons.qr_code_scanner,
-                  size: 40,
-                ),
-              ),
+          title: Text(widget.title),
+        ),
+        body: Center(
+            // Center is a layout widget. It takes a single child and positions it
+            // in the middle of the parent.
+            child: FractionallySizedBox(
+          heightFactor: 0.6,
+          widthFactor: 0.8,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              ...pages
+                  .map((page) => ElevatedButton(
+                      onPressed: () =>
+                          Navigator.pushNamed(context, page["route"]!),
+                      child: Text(page["pageTitle"]!)))
+                  .toList()
             ],
           ),
-          title: Text(
-            '  QR / Barcode Scanner',
-          ),
-        ),
-        body: TabBarView(
-          children: [
-            First(),
-            Second(),
-          ],
-        ),
-      ),
-    );
+        )) // This trailing comma makes auto-formatting nicer for build methods.
+        );
   }
 }
