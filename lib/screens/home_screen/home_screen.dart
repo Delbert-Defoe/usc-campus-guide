0d87f97024157db785f94d705f0c43acc71600ab
ShemarB <114029006+ShemarB@users.noreply.github.com>
2023-03-21 01:48:18 -0400

    Update home_screen.dart

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -9,6 +9,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int currentIndex = 0;
+  final screens = [];
 
   @override
   Widget build(BuildContext context) {
@@ -73,9 +74,104 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ),
             ),
-          )
+          ),
+          Positioned(
+            // learnaboutthebuildingsaroundyo (504:462)
+            left: 16,
+            top: 214,
+            child: Align(
+              child: SizedBox(
+                width: 203,
+                height: 32,
+                child: Text(
+                    'Learn about the buildings around you, never get lost',
+                    style: texttheme.bodySmall),
+              ),
+            ),
+          ),
+          Positioned(
+              left: 10,
+              top: 262,
+              child: Container(
+                width: 328,
+                height: 439,
+                child: Container(
+                  width: double.infinity,
+                  height: 358,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.fromLTRB(0, 0, 8, 0),
+                        width: 104,
+                        height: double.infinity,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Container(
+                              child: Material(
+                                borderRadius: BorderRadius.circular(10),
+                                clipBehavior: Clip.antiAliasWithSaveLayer,
+                                child: InkWell(
+                                    onTap: () {},
+                                    child: Ink.image(
+                                      image: NetworkImage(
+                                          'https://toplist.info/images/800px/university-of-the-southern-caribbean-819839.jpg'),
+                                      height: 88,
+                                      width: double.infinity,
+                                      fit: BoxFit.cover,
+                                    )),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 8,
+                            ),
+                            Container(
+                              child: Material(
+                                borderRadius: BorderRadius.circular(10),
+                                clipBehavior: Clip.antiAliasWithSaveLayer,
+                                child: InkWell(
+                                    onTap: () {},
+                                    child: Ink.image(
+                                      image: NetworkImage(
+                                          'https://toplist.info/images/800px/university-of-the-southern-caribbean-819839.jpg'),
+                                      height: 166,
+                                      width: double.infinity,
+                                      fit: BoxFit.cover,
+                                    )),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 8,
+                            ),
+                            Container(
+                              child: Material(
+                                borderRadius: BorderRadius.circular(10),
+                                clipBehavior: Clip.antiAliasWithSaveLayer,
+                                child: InkWell(
+                                    onTap: () {},
+                                    child: Ink.image(
+                                      image: NetworkImage(
+                                          'https://toplist.info/images/800px/university-of-the-southern-caribbean-819839.jpg'),
+                                      height: 88,
+                                      width: double.infinity,
+                                      fit: BoxFit.cover,
+                                    )),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )),
         ],
       )),
+      //body: screens[currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: currentIndex,
         onTap: (index) => setState(() => currentIndex = index),
