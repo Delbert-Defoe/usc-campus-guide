e76d0d3c52e409b65f790b6bc9979f5b382262ff
liamkirt <liamkirt1@gmail.com>
2023-03-28 10:12:51 -0400

    Update screen_switcher.dart

--- lib/screens/floor_plan/screen_switcher.dart
+++ lib/screens/floor_plan/screen_switcher.dart
@@ -1,9 +1,62 @@
 import 'package:flutter/material.dart';
 
+int ScreenOption = 0;
+
 class ScreenSwitcher extends StatefulWidget {
   final List<Widget> screens;
+  final List<Widget> screens1;
+  final List<Widget> screens2;
+  final List<Widget> screens3;
+  final List<Widget> screens4;
+  final List<Widget> screens5;
+  final List<Widget> screens6;
+  final List<Widget> screens7;
+  final List<Widget> screens8;
+  final List<Widget> screens9;
+  final List<Widget> screens10;
+  final List<Widget> screens11;
+  final List<Widget> screens12;
+  final List<Widget> screens13;
+  final List<Widget> screens14;
+  final List<Widget> screens15;
 
-  ScreenSwitcher({required this.screens});
+  final List<List<Widget>> allScreens;
+
+  ScreenSwitcher({
+    required this.screens,
+    required this.screens1,
+    required this.screens2,
+    required this.screens3,
+    required this.screens4,
+    required this.screens5,
+    required this.screens6,
+    required this.screens7,
+    required this.screens8,
+    required this.screens9,
+    required this.screens10,
+    required this.screens11,
+    required this.screens12,
+    required this.screens13,
+    required this.screens14,
+    required this.screens15,
+  }) : allScreens = [
+         screens,
+         screens1,
+         screens2,
+         screens3,
+         screens4,
+         screens5,
+         screens6,
+         screens7,
+         screens8,
+         screens9,
+         screens10,
+         screens11,
+         screens12,
+         screens13,
+         screens14,
+         screens15,
+       ];
 
   @override
   _ScreenSwitcherState createState() => _ScreenSwitcherState();
@@ -30,10 +83,13 @@ class _ScreenSwitcherState extends State<ScreenSwitcher> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+
+    int index = 3;
+
+      return Scaffold(
       body: PageView(
-        children: widget.screens,
-        controller: _pageController,
+      children: widget.allScreens[index],
+      controller: _pageController,
       ),
       bottomNavigationBar: BottomAppBar(
         child: Row(
@@ -45,7 +101,7 @@ class _ScreenSwitcherState extends State<ScreenSwitcher> {
             ),
             IconButton(
               icon: Icon(Icons.arrow_forward),
-              onPressed: _currentIndex == widget.screens.length - 1 ? null : _incrementIndex,
+              onPressed: _currentIndex == widget.allScreens[index].length - 1 ? null : _incrementIndex,
             ),
           ],
         ),
