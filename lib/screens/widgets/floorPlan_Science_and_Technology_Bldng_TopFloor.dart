9689476c155f4b4e3baa893cb7d558c9dfa413f7
liamkirt <liamkirt1@gmail.com>
2023-03-28 10:13:36 -0400

    Update floorPlan_Science_and_Technology_Bldng_TopFloor.dart

--- lib/screens/widgets/floorPlan_Science_and_Technology_Bldng_TopFloor.dart
+++ lib/screens/widgets/floorPlan_Science_and_Technology_Bldng_TopFloor.dart
@@ -3,10 +3,10 @@ import 'package:ucg/main.dart';
 import 'dart:ui' as ui;
 
 void main() {
-  runApp(MyApp());
+  runApp(floorPlan_Science_and_Technology_Bldng_TopFloor());
 }
 
-class MyApp extends StatelessWidget {
+class floorPlan_Science_and_Technology_Bldng_TopFloor extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
