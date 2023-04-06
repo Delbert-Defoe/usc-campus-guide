9acbbf0019cc603c777bc4b8d5e32f4f1cb45f43
ShemarB <114029006+ShemarB@users.noreply.github.com>
2023-04-05 20:58:24 -0400

    Update home_screen.dart

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -20,34 +20,35 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Stack(
           children: [
             Positioned(
-              left: 16,
-              top: 35,
-              child: SizedBox(
-                width: 325,
-                height: 32,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.only(right: 193),
-                        child: Text(
-                          'Home',
-                          style: texttheme.bodyLarge,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      constraints: const BoxConstraints(maxWidth: 83),
-                      child: Text(
-                        'USC,\nMaracas Valley',
-                        style: texttheme.bodySmall,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+  left: 16,
+  top: 35,
+  child: SizedBox(
+    width: MediaQuery.of(context).size.width - 32, // Use dynamic width
+    height: 32,
+    child: Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Expanded(
+          child: Text(
+            'Home',
+            style: texttheme.bodyLarge,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 8.0), // Add margin to text
+          child: Container(
+            constraints: const BoxConstraints(maxWidth: 83),
+            child: Text(
+              'USC,\nMaracas Valley',
+              style: texttheme.bodySmall,
             ),
+          ),
+        ),
+      ],
+    ),
+  ),
+),
+
             Positioned(
               left: 16,
               top: 94,
@@ -94,87 +95,184 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
             Positioned(
-              left: 0,
+              left: 16,
               top: 84 + screenWidth / 3 + 16 + screenHeight * 0.06 + 16,
               child: SizedBox(
-                width: screenWidth,
-                height: screenHeight -
-                    (94 + screenWidth / 3 + 16 + screenHeight * 0.06 + 16),
-                child: GridView.count(
-                  crossAxisCount: 3,
-                  mainAxisSpacing: 16,
-                  crossAxisSpacing: 16,
-                  padding: const EdgeInsets.all(16),
-                  childAspectRatio: 1,
-                  physics: const NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const ErrorScreen()));
-                      },
-                      child: Text('Admin'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const ErrorScreen()));
-                      },
-                      child: Text('USC Auditorium'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const ErrorScreen()));
-                      },
-                      child: Text('School of Social Sciences'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const ErrorScreen()));
-                      },
-                      child: Text('School of Business'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const ErrorScreen()));
-                      },
-                      child: Text('School of Sci and Tech'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const ErrorScreen()));
-                      },
-                      child: Text('School of Theolgy'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const ErrorScreen()));
-                      },
-                      child: Text('School of Humanities'),
-                    ),
-                  ],
-                ),
-              ),
+                  width: screenWidth,
+                  height: screenHeight -
+                      (94 + screenWidth / 3 + 16 + screenHeight * 0.06 + 16),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                            margin: EdgeInsets.fromLTRB(0, 0, 8, 0),
+                            width: MediaQuery.of(context).size.width * 0.25,
+                            height: double.infinity,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Material(
+                                  borderRadius: BorderRadius.circular(10),
+                                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                                  child: InkWell(
+                                    onTap: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  const ErrorScreen()));
+                                    },
+                                    child: Ink.image(
+                                      image: const AssetImage(
+                                          'assets/admin_bld.jpg'),
+                                      height:
+                                          MediaQuery.of(context).size.height *
+                                              0.1,
+                                      width: double.infinity,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 8,
+                                ),
+                                Material(
+                                  borderRadius: BorderRadius.circular(10),
+                                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                                  child: InkWell(
+                                    onTap: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  const ErrorScreen()));
+                                    },
+                                    child: Ink.image(
+                                      image: const AssetImage(
+                                          'assets/humanities_bld.jpg'),
+                                      height:
+                                          MediaQuery.of(context).size.height *
+                                              0.2,
+                                      width: double.infinity,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 8,
+                                ),
+                                Material(
+                                  borderRadius: BorderRadius.circular(10),
+                                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                                  child: InkWell(
+                                    onTap: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  const ErrorScreen()));
+                                    },
+                                    child: Ink.image(
+                                      image: const AssetImage(
+                                          'assets/humanities_bld.jpg'),
+                                      height:
+                                          MediaQuery.of(context).size.height *
+                                              0.1,
+                                      width: double.infinity,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.3,
+                            height: double.infinity,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                  width: double.infinity,
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.15,
+                                  child: Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      Container(
+                                        margin: EdgeInsets.fromLTRB(0, 0, 8, 0),
+                                        width:
+                                            MediaQuery.of(context).size.width *
+                                                0.2,
+                                        height: double.infinity,
+                                        child: Material(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                          clipBehavior:
+                                              Clip.antiAliasWithSaveLayer,
+                                          child: InkWell(
+                                              onTap: () {
+                                                Navigator.push(
+                                                    context,
+                                                    MaterialPageRoute(
+                                                        builder: (context) =>
+                                                            const ErrorScreen()));
+                                              },
+                                              child: Ink.image(
+                                                image: const AssetImage(
+                                                    'assets/admin_bld.jpg'),
+                                                height: MediaQuery.of(context)
+                                                        .size
+                                                        .height *
+                                                    0.2,
+                                                width: double.infinity,
+                                                fit: BoxFit.cover,
+                                              )),
+                                        ),
+                                      ),
+                                      Expanded(
+                                        child: SizedBox(
+                                          width: MediaQuery.of(context)
+                                                  .size
+                                                  .width -
+                                              45,
+                                          height: double.infinity,
+                                          child: Material(
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                            clipBehavior:
+                                                Clip.antiAliasWithSaveLayer,
+                                            child: InkWell(
+                                              onTap: () {
+                                                Navigator.push(
+                                                  context,
+                                                  MaterialPageRoute(
+                                                      builder: (context) =>
+                                                          const ErrorScreen()),
+                                                );
+                                              },
+                                              child: Ink.image(
+                                                image: const AssetImage(
+                                                    'assets/humanities_bld.jpg'),
+                                                height: 114,
+                                                width: double.infinity,
+                                                fit: BoxFit.cover,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    
+                  ),
             ),
           ],
         ),
